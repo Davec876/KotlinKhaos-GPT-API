beae25a0abef5fd38bedb2062e7e85e25653807c
Maximo Guk <maximo.guk@dal.ca>
2023-09-24 16:52:41 -0300

    Initalized Repository

---
+++ src/index.ts
@@ -0,0 +1,115 @@
+/**
+ * Welcome to Cloudflare Workers!
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+import {
+	error,
+	IRequest, // creates error responses
+	json, // creates JSON responses
+	Router, // the ~440 byte router itself
+	withParams, // middleware: puts params directly on the Request
+} from 'itty-router';
+import Conversation from './classes/Conversation';
+
+export interface Env {
+	OPENAI_API_TOKEN: string;
+	CONVERSATIONS: KVNamespace;
+}
+
+// create a new Router
+const router = Router();
+
+router
+	// add some middleware upstream on all routes
+	.all('*', withParams)
+
+	// POST GPT create a new conversation utilizing a prompt
+	.POST('/android-problem/:userId', async (req, env: Env) => {
+		const url = new URL(req.url);
+		const userId = req.params.userId;
+		const prompt = url.searchParams.get('prompt');
+
+		if (!prompt) {
+			return error(400, 'No prompt specified!');
+		}
+
+		if (prompt.length > 20) {
+			return error(400, 'Prompt is too long');
+		}
+
+		const conversation = await Conversation.newConversation(env, userId, prompt);
+
+		return { answer: conversation.getLatestContent(), conversationId: conversation.getId() };
+	})
+
+	// GET GPT conversation by Id
+	.get('/android-problem/:userId/:conversationId', async (req, env: Env) => {
+		const conversation = await Conversation.getConversation(env, req.params.conversationId);
+
+		if (!conversation) {
+			return error(404, 'No conversation by that Id found');
+		}
+
+		return { answer: conversation.getLatestContent() };
+	})
+
+	// POST GPT give feedback to a user's answer
+	.POST('/android-problem/:userId/:conversationId', async (req, env: Env) => {
+		const conversationId = req.params.conversationId;
+
+		// TODO: Add typeguard later
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const body: any = await req.json().catch(() => error(400, 'Expect JSON body'));
+		const userAnswer: string = body.answer;
+
+		if (!userAnswer) {
+			return error(400, 'No answer specified!');
+		}
+
+		if (userAnswer.length > 300) {
+			return error(400, 'Please shorten your answer');
+		}
+
+		const conversation = await Conversation.getConversation(env, conversationId);
+
+		if (!conversation) {
+			return error(404, 'No conversation by that Id found');
+		}
+
+		const feedback = await conversation.giveFeedback(env, userAnswer);
+
+		return { feedback };
+	})
+
+	// POST GPT continue a conversation
+	.POST('/android-problem/:userId/:conversationId/continue', async (req, env: Env) => {
+		const conversationId = req.params.conversationId;
+
+		const conversation = await Conversation.getConversation(env, conversationId);
+
+		if (!conversation) {
+			return error(404, 'No conversation by that Id found');
+		}
+
+		const feedback = await conversation.continue(env);
+
+		return { feedback };
+	})
+
+	// 404 for everything else
+	.all('*', () => error(404));
+
+// Example: Cloudflare Worker module syntax
+export default {
+	fetch: (req: IRequest, env: Env) =>
+		router
+			.handle(req, env)
+			.then(json) // send as JSON
+			.catch(error), // catch errors
+};
---
+++ src/services/openAi.ts
@@ -0,0 +1,78 @@
+import { OpenAI } from 'openai';
+import type { Env } from '../index';
+import type { ChatCompletionMessage } from 'openai/resources/chat/completions';
+import Conversation from '../classes/Conversation';
+
+function getStartingMessage(prompt: string): ChatCompletionMessage[] {
+	return [
+		{
+			content:
+				'For the following questions, reply with only one numbered android interview-type knowledge question. This is for a university student taking a course about the principles of mobile computing and the concepts and techniques underlying the design and development of mobile computing applications utilizing Kotlin.',
+			role: 'system',
+		},
+		{
+			content: `With a particular focus on ${prompt}`,
+			role: 'user',
+		},
+	];
+}
+
+function giveFeedbackMessage(history: ChatCompletionMessage[], userAnswer: string): ChatCompletionMessage[] {
+	const feedbackMessage: ChatCompletionMessage[] = [];
+	const systemMessage: ChatCompletionMessage = {
+		content:
+			"A university student who is taking a course about the principles of mobile computing is replying to a question that you've just given them in a numbered quiz format. You are replying to them and giving them serious concise feedback within 50 words on their answer and assigning a score out of 10 in the format 'Score: score/10'",
+		role: 'system',
+	};
+	const userMessage: ChatCompletionMessage = {
+		content: userAnswer,
+		role: 'user',
+	};
+
+	feedbackMessage.push(systemMessage);
+	feedbackMessage.concat(history);
+	feedbackMessage.push(userMessage);
+	return feedbackMessage;
+}
+
+export async function createNewConversation(env: Env, prompt: string): Promise<ChatCompletionMessage> {
+	const openai = new OpenAI({ apiKey: env.OPENAI_API_TOKEN });
+	const completion = await openai.chat.completions.create({
+		model: 'gpt-4',
+		messages: getStartingMessage(prompt),
+		max_tokens: 50,
+	});
+
+	return completion.choices[0].message;
+}
+
+export async function continueConversation(conversation: Conversation, env: Env): Promise<string | null> {
+	const openai = new OpenAI({ apiKey: env.OPENAI_API_TOKEN });
+	const message = getStartingMessage(conversation.getPrompt()).concat(conversation.getHistory());
+
+	const completion = await openai.chat.completions.create({
+		model: 'gpt-4',
+		messages: message,
+		max_tokens: 50,
+	});
+
+	await conversation.appendMessageToHistory(env, completion.choices[0].message);
+	return conversation.getLatestContent();
+}
+
+export async function giveFeedbackToConversation(conversation: Conversation, userAnswer: string, env: Env): Promise<string | null> {
+	const openai = new OpenAI({ apiKey: env.OPENAI_API_TOKEN });
+	const message = giveFeedbackMessage(conversation.getHistory(), userAnswer);
+
+	const completion = await openai.chat.completions.create({
+		model: 'gpt-4',
+		messages: message,
+		max_tokens: 100,
+	});
+
+	// Combine user message and completion message from GPT
+	const messages = [message[message.length - 1], completion.choices[0].message];
+
+	await conversation.appendMessagesToHistory(env, messages);
+	return conversation.getLatestContent();
+}
