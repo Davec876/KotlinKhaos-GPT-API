e589fb9f036ed7e8c31ba345949db75a7356ba6a
Maximo Guk <maximo.guk@dal.ca>
2023-10-01 13:27:48 -0300

    Update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,5 +3,9 @@ module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'@typescript-eslint/consistent-type-imports': 'error',
+	},
 	root: true,
 };
